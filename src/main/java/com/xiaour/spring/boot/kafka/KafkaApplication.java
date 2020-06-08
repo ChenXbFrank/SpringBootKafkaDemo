9f5ac6dc62a99519d0450d9a62f372b5a6a852a5
ChenXbFrank <810464826@qq.com>
2020-06-08 21:48:38 +0800

    添加lombok

--- src/main/java/com/xiaour/spring/boot/kafka/KafkaApplication.java
+++ src/main/java/com/xiaour/spring/boot/kafka/KafkaApplication.java
@@ -1,7 +1,7 @@
 package com.xiaour.spring.boot.kafka;
 
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaApplication {
--- src/main/java/com/xiaour/spring/boot/kafka/consumer/Consumer.java
+++ src/main/java/com/xiaour/spring/boot/kafka/consumer/Consumer.java
@@ -6,25 +6,16 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-/**
- * @Author: Xiaour
- * @Description:
- * @Date: 2018/5/22 15:03
- */
 @Component
 public class Consumer {
 
     @KafkaListener(topics = {"test"})
     public void listen(ConsumerRecord<?, ?> record){
-
         Optional<?> kafkaMessage = Optional.ofNullable(record.value());
-
         if (kafkaMessage.isPresent()) {
-
             Object message = kafkaMessage.get();
-            System.out.println("---->"+record);
-            System.out.println("---->"+message);
-
+            System.out.println("record---->"+record);
+            System.out.println("message---->"+message);
         }
 
     }
